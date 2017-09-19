fbfce4e6927d06692fc33fa0c35df4157197c207
Ismael G Marin C <ismael.marin@gmail.com>
2017-09-18 23:44:32 -0500

    Information base

--- Part2/DatabaseExample/db_connection.rb
+++ Part2/DatabaseExample/db_connection.rb
@@ -16,6 +16,19 @@ end
 class Store < ActiveRecord::Base
 end
 
-my_products = Store.all
-pp my_products
-puts my_products.count
+if __FILE__ == $0
+  Product.create(id: 1, name: 'Soap')
+  Product.create(id: 2, name: 'Paper')
+  Product.create(id: 3, name: 'Rock')
+  Product.create(id: 4, name: 'Scissors')
+  Product.create(id: 5, name: 'Cellphone')
+  Store.create(id: 1, location: 'Guanajuato')
+  Store.create(id: 2, location: 'Leon')
+  puts " Base information ".center(50, '*')
+
+  rand(1..40).times do |index|
+    Purchase.create(id: index + 1, product_id: rand(1..5), store_id: rand(1..2))
+  end
+  puts " Purchases created ".center(50, '*')
+
+end
